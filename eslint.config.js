0195002230a2cbc02442242206b88dce3c8befe2
Renzo Lucioni <renzolucioni@gmail.com>
2025-10-18 12:20:13 -0400

    browser globals for browser code

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
-import globals from 'globals';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
 import js from '@eslint/js';
 import { includeIgnoreFile } from '@eslint/compat';
 import { fileURLToPath } from 'node:url';
@@ -9,6 +9,7 @@ const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
 export default defineConfig([
   includeIgnoreFile(gitignorePath, 'imported .gitignore patterns'),
   globalIgnores(['**/*.min.js']),
-  { files: ['**/*.js'], languageOptions: { globals: globals.node } },
+  { files: ['**/*.js'], ignores: ['src/assets/js/**/*.js'], languageOptions: { globals: globals.node } },
+  { files: ['src/assets/js/**/*.js'], languageOptions: { globals: globals.browser } },
   { files: ['**/*.js'], plugins: { js }, extends: ['js/recommended'] },
 ]);
